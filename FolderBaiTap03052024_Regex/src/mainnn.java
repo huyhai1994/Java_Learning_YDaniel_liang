d78a10fb4b847c0f40e7fc124b1431cfaa0a4bd6
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-05-03 16:24:01 +0700

    dell_huyhai1994

---
+++ FolderBaiTap03052024_Regex/src/mainnn.java
@@ -0,0 +1,9 @@
+public class mainnn {
+    public static void main(String[] args) {
+        String vietTelSampleString = "03-9226-0046";
+        
+        System.out.println(vietTelSampleString.matches("[\\d]{2}-[\\d]{4}-[\\d]{4}"));
+
+    }
+    
+}
